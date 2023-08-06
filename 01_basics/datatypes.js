18e0472886c1b41b1360ba7325296943f0ac0dd3
Deepak Bagati <130148122+deepakbagati@users.noreply.github.com>
2023-08-06 14:05:34 +0000

    01 basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,7 @@
+let name ="Hello"
+let age =18
+//number,string,boolean,null> standalone value,undefined,symbol.unique
+
+console.log(typeof "Hello")
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accounts = 1234
+let accountID= "123456"
+var accountIDs="93020"
+let pie
+
+console.table([accounts,accountID,accountIDs])
