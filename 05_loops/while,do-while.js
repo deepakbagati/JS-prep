8f9613d386905212b6e01ac636dfbd3e4f562818
Deepak Bagati <130148122+deepakbagati@users.noreply.github.com>
2023-08-09 17:43:18 +0000

    container config done

---
+++ 05_loops/while,do-while.js
@@ -0,0 +1,32 @@
+
+// let index = 0
+// // while (index <= 10) {
+// //     console.log(`Value of index is ${index}`);
+// //     index = index + 2
+// // }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+
+let myarr=[ "deepak","chetan","thakur"]
+let arr2=0;
+console.log(myarr.length)
+while(arr2<myarr.length){
+        console.log(`Value is ${myarr[arr2]}`);
+    arr2=arr2 + 1
+}
+
+
